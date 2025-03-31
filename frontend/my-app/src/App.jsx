d213b27e13b471c641b7929c49ea9191add59fbf
Ayush Tripathi <ayush.bhrg@gmail.com>
2025-04-01 01:17:51 +0530

    Hello

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_URL = "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export default function App() {
   const [title, setTitle] = useState("");
