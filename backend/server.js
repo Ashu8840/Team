0545419e94d95b5cecafdf60b961f45e6f920ac4
Ayush Tripathi <ayush.bhrg@gmail.com>
2025-04-01 01:26:11 +0530

    New commit

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use(express.json());
 // };
 
 app.use(cors({
-  origin:"http://localhost:5173" ,
+  origin:"https://team-gilt-sigma.vercel.app/" ,
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
